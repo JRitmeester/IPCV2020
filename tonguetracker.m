8265741ddfb9909cc148fbd472534d4cb53f1304
Protik Banerji <protik09@gmail.com>
2020-04-01 18:16:08 +0200

    Switch to deplyable video player and add writing video to disk

--- tonguetracker.m
+++ tonguetracker.m
@@ -10,7 +10,7 @@ addpath(genpath('.'));
 
 
 videoReader = VideoReader('subjects\subject1\proefpersoon 1.2_M.avi');
-videoPlayer = vision.VideoPlayer;
+videoPlayer = vision.DeployableVideoPlayer;
 
 frame = readFrame(videoReader);
 imshow(frame);
@@ -43,47 +43,60 @@ points = points.Location;
 
 frame = insertShape(frame,'Rectangle',objectRegion,'Color','red');
 frame = insertMarker(frame, points,'+', 'Color', 'green');
-% figure; imshow(frame);
+% figure;
+imshow(frame);
 
 pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
 initialize(pointTracker, points, frame);
 oldPoints = points;
 
+% [img_x, img_y] = size(frame);
+% image_storage = cell(videoReader.NumFrames, img_x*img_y, 3);
+image_counter = 1;
 while hasFrame(videoReader)
     % get the next frame
     frame = readFrame(videoReader);
 
     % Track the points. Note that some points may be lost.
-    [points, isFound] = step(pointTracker, frame);
+    [points, isFound] = pointTracker(frame);
     visiblePoints = points(isFound, :);
     oldInliers = oldPoints(isFound, :);
-    
+
     if size(visiblePoints, 1) >= 2 % need at least 2 points
-        
+
         % Estimate the geometric transformation between the old points
         % and the new points and eliminate outliers
         [xform, oldInliers, visiblePoints] = estimateGeometricTransform(...
             oldInliers, visiblePoints, 'similarity', 'MaxDistance', 4);
-        
+
         % Apply the transformation to the bounding box points
 %         bboxPoints = transformPointsForward(xform, bboxPoints);
-                
+
 %         % Insert a bounding box around the object being tracked
 %         bboxPolygon = reshape(bboxPoints', 1, []);
 %         frame = insertShape(frame, 'Polygon', bboxPolygon, ...
 %             'LineWidth', 2);
-%                 
+%
         % Display tracked points
         gFrame = gpuArray(frame);
         color_corected_frame = chrom_adapt(gFrame,c);
-        frame = insertMarker(gather(color_corected_frame), visiblePoints, '+', ...
-            'Color', 'white');       
-        
+
+        image_storage{image_counter} = insertMarker(gather(color_corected_frame), visiblePoints, '+', ...
+            'Color', 'white');
+
         % Reset the points
         oldPoints = visiblePoints;
-        setPoints(pointTracker, oldPoints);        
+        setPoints(pointTracker, oldPoints);
+        %     Display the annotated video frame using the video player object
+        videoPlayer(image_storage{image_counter});
     end
-    
-%     Display the annotated video frame using the video player object
-    step(videoPlayer, frame)
+
+image_counter = image_counter + 1;
 end
+v = VideoWriter('newfile.avi','Uncompressed AVI');
+open(v);
+for index = 1: videoReader.NumFrames - 1
+   writeVideo(v,image_storage{index});
+end
+close();
+
