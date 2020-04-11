443f1310f85cb329a1337007f3580aaa286621de
Ayham Alharbat <a.n.alharbat@gmail.com>
2020-04-11 18:49:20 +0200

    Demo for Triangulation

---
+++ Demo_Trangulate/Demo_trangulate.m
@@ -0,0 +1,5 @@
+load("stereoParams_Cam1M_L.mat")
+load("stereoParams_Cam1M_R.mat")
+load("stereoParams_Cam1L_R.mat")
+load('Paths_RML_withTracker.mat')
+create3DPath(pathM, pathL, stereoParams_Cam1M_L)
