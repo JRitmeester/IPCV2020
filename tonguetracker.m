654825eea9d2cb1d03fcd95958810b9cca4b1f42
Jeroen <jeroenritmeester1407@gmail.com>
2020-04-11 13:03:52 +0200

    Turned triangulation into function create3Dpath.m

--- tonguetracker.m
+++ tonguetracker.m
@@ -11,78 +11,19 @@ videoPlayer = vision.VideoPlayer;
 
 frame = readFrame(videoReader);
 figure(1); imshow(frame);
-%%
 
-% [x,y] = getpts;
-% temp = frame(round(x),round(y),:);
-% r = temp(1,1,1);
-% g = temp(1,1,2);
-% b = temp(1,1,3);
-% c = [r g b];
+load stereoCameraCalibrations/stereoParamsLM stereoParams;
 
-load c c;
-%%
-frameCounter = 100;
+%% Insert Ayham's code that produces 2 paths
+% Now mimicked by loading the saved paths
+load path_tongue_left
+load path_tongue_middle
+load path_tracker_left
+load path_tracker_middle
 
-% while hasFrame(videoReader)
-%    frame = read(videoReader, frameCounter);
-%    frame = chromadapt(frame,c);
-%    step(videoPlayer, frame);
-%    frameCounter = frameCounter + 1;
-% end
+path1 = path_tongue_left;
+path2 = path_tongue_middle;
 
-%%
+%% Create 3D path using triangulation
+create3DPath(path1, path2, stereoParams);
 
-rect = drawrectangle;
-
-objectRegion = round(rect.Position);
-points = detectMinEigenFeatures(rgb2gray(frame), 'ROI', objectRegion);
-points = points.Location;
-
-frame = insertShape(frame,'Rectangle',objectRegion,'Color','red');
-frame = insertMarker(frame, points,'+', 'Color', 'green');
-% figure; imshow(frame);
-
-pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
-initialize(pointTracker, points, frame);
-oldPoints = points;
-
-close(figure(1));
-
-while hasFrame(videoReader)
-    % get the next frame
-    frame = readFrame(videoReader);
-
-    % Track the points. Note that some points may be lost.
-    [points, isFound] = step(pointTracker, frame);
-    visiblePoints = points(isFound, :);
-    oldInliers = oldPoints(isFound, :);
-    
-    if size(visiblePoints, 1) >= 2 % need at least 2 points
-        
-        % Estimate the geometric transformation between the old points
-        % and the new points and eliminate outliers
-        [xform, oldInliers, visiblePoints] = estimateGeometricTransform(...
-            oldInliers, visiblePoints, 'similarity', 'MaxDistance', 4);
-        
-        % Apply the transformation to the bounding box points
-%         bboxPoints = transformPointsForward(xform, bboxPoints);
-                
-%         % Insert a bounding box around the object being tracked
-%         bboxPolygon = reshape(bboxPoints', 1, []);
-%         frame = insertShape(frame, 'Polygon', bboxPolygon, ...
-%             'LineWidth', 2);
-%                 
-        % Display tracked points
-%         frame = chromadapt(frame,c);
-        frame = insertMarker(frame, visiblePoints, '+', ...
-            'Color', 'white');       
-        
-        % Reset the points
-        oldPoints = visiblePoints;
-        setPoints(pointTracker, oldPoints);        
-    end
-    
-%     Display the annotated video frame using the video player object
-    step(videoPlayer, frame)
-end
