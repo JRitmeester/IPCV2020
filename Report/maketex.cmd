d1a67139ea6772fd70562b5d5b5857cbe9bda79d
Protik Banerji <protik09@gmail.com>
2020-04-13 09:06:56 +0200

    Update tex file with name and windows script to compile tex

---
+++ Report/maketex.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+:: List all tex files in the directory without fancy headers and write to variable
+dir *.tex /s /b /o:gn > tempfile
+:: Stupid batch hack to assign value to variable from command output
+set /p tex_file= < tempfile
+del tempfile
+:: Convert Latex to PDF
+pdflatex "%tex_file%"
+
+:: Wait for user input
+PAUSE
+
+@echo on
